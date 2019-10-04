2b4e24fe64c343c38295ece3451e605cde030490
Senthil <senthilprabhut@gmail.com>
2019-10-04 11:22:50 -0700

    Update action1-67dde0d344562657443633598761c14ab54163c4c04df54c1b7e180e.py

--- action1/action1-67dde0d344562657443633598761c14ab54163c4c04df54c1b7e180e.py
+++ action1/action1-67dde0d344562657443633598761c14ab54163c4c04df54c1b7e180e.py
@@ -1,5 +1,5 @@
 def handler(context, inputs):
-    greeting = "NiHao, {0}!".format(inputs["target"])
+    greeting = "Vanakkam, {0}!".format(inputs["target"])
     print(greeting)
 
     outputs = {
