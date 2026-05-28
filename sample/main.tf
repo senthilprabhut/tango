b4670b297211b7ad2dfab4f6d907f91493992356
Senthil <senthilprabhut@gmail.com>
2026-05-28 12:55:20 -0700

    Example terraform template

---
+++ sample/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.5.5"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.0"
+    }
+  }
+}
+
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
+}
+
+output "generated_number" {
+  value       = random_integer.suffix.result
+  description = "The generated random 4-digit integer"
+}
